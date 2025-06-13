50b97358e67a9b4268e1ff06c5d87cb8171badfa
Nascent7 <mahquintana@gmail.com>
2025-06-13 01:27:16 -0400

    updated main to address verbose system flag, and to handle printing response and tokens based off of the presence of the verbose flag

--- Main/main.py
+++ Main/main.py
@@ -1,17 +1,40 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import sys
+from google.genai import types
 
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
+def main():
+    load_dotenv()
 
+# load API
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
 
-client = genai.Client(api_key=api_key)
+# user input 
+    user_prompt = " ".join(arg for arg in sys.argv[1:] if arg != "--verbose")
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]   
 
-response = client.models.generate_content(
-    model="gemini-2.0-flash-001", contents="Why is Boot.dev such a good place to learn backend development? Use one paragraph maximum."
-)
-print(response.text)
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+# system prompt missing error handling
+    if not user_prompt.strip():
+        print("Missing prompt!")
+        sys.exit(1)
+
+# model response
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001", contents=messages,
+    )
+    
+# general printed outupt
+    print(response.text)
+
+# verbose tag handling
+    if "--verbose" in sys.argv:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+main()
